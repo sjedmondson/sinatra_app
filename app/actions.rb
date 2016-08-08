7ccd40a6dcba4f8b324230bf48cda6dc7939cbc3
vagrant <vagrant@lighthouse-labs>
2016-08-08 00:25:39 +0000

    pre enhance commit

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,37 @@
+require 'pry'
+
 # Homepage (Root path)
 get '/' do
   erb :index
+end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end 
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
+
+get '/messages/:id' do
+  @message = Message.find_by params[:id]
+  erb :'messages/show'
+end
+
+get '/messages/show' do
+  erb :'messages/show'
 end
---
+++ app/models/message.rb
@@ -0,0 +1,7 @@
+class Message < ActiveRecord::Base
+
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+
+end
